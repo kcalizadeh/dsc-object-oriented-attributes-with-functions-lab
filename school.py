2763ff8a766cf42f1f758ea18adfbedb7e1e3837
Kourosh Alizadeh <kcalizadeh@gmail.com>
2020-10-19 11:56:46 -0600

    finished lab

--- school.py
+++ school.py
@@ -1,1 +1,18 @@
 class School:
+    def __init__(self, name, roster={}):
+        self.name = name
+        self.roster = roster
+
+    def add_student(self, student_name, grade):
+        if grade not in self.roster.keys():
+            self.roster[grade] = [student_name]
+        else:
+            self.roster[grade].append(student_name)
+    
+    def grade(self, grade):
+        return self.roster[grade]
+
+    def sort_roster(self):
+        for grade in self.roster.keys():
+            self.roster[grade].sort()
+        return self.roster
